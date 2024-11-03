8673ed0019e01560e11f2dd15c0b3469f72b7f9b
Haitem <haitem.bourahla7@gmail.com>
2024-11-03 09:39:52 +0100

    added google fetching sheets

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -22,7 +22,35 @@ const Navbar = () => {
     const handleThoughtsClick = () => {
         dispatch(getPosts());
     }
-    
+
+    const loadGoogleDriveClient = () => {
+        window.gapi.load("client", async () => {
+            await window.gapi.client.setApiKey("YOUR_API_KEY");
+            await window.gapi.client.setToken({ access_token: user.token });
+            
+            window.gapi.client.load("https://www.googleapis.com/discovery/v1/apis/drive/v3/rest")
+                .then(() => console.log("GAPI client loaded for Drive API"))
+                .catch(err => console.error("Error loading GAPI client:", err));
+        });
+    };
+
+    const fetchGoogleSheets = async () => {
+        try {
+            const response = await window.gapi.client.drive.files.list({
+                q: "mimeType='application/vnd.google-apps.spreadsheet'"
+            });
+            console.log("Response:", response.result.files);
+        } catch (error) {
+            console.error("Error fetching Google Sheets:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (user) {
+            loadGoogleDriveClient();
+        }
+    }, [user]);
+
     return (
     <AppBar className={classes.appBar} color='inherit'>
          <Grid className={classes.mainContainer} container >
@@ -34,10 +62,15 @@ const Navbar = () => {
             </Grid>
             <Toolbar className={classes.toolbar} >
                 { user ? (
-                    <Container className={classes.profile} component={Link} to={'/thoughts/profile'}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.picture}>{user.result.name.charAt(0)}</Avatar>
-                        <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
-                    </Container>
+                    <>
+                        <Container className={classes.profile} component={Link} to={'/thoughts/profile'}>
+                            <Avatar className={classes.purple} alt={user.result.name} src={user.result.picture}>{user.result.name.charAt(0)}</Avatar>
+                            <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+                        </Container>
+                        <Button className={classes.apiButton} variant='contained' color='secondary' onClick={fetchGoogleSheets}>
+                                Fetch Google Sheets
+                        </Button>
+                    </>
                 ) : (
                     <Button className={classes.signin} component={Link} to='/thoughts/auth' variant='contained' color='primary'>Sign In</Button>
                 ) }
